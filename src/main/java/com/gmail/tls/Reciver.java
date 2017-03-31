8b9d2d0d894d7b3db3edca6baee1dca39ebbc41a
Iryna Kazantseva <irynakazantseva8@gmail.com>
2017-03-31 09:35:48 -0700

    sender ssl/tls, reciver pop3

--- src/main/java/com/gmail/tls/Reciver.java
+++ src/main/java/com/gmail/tls/Reciver.java
@@ -2,35 +2,38 @@ package com.gmail.tls;
 
 import java.io.IOException;
 import java.util.Properties;
+import java.util.regex.Pattern;
+
 import javax.mail.*;
+import javax.mail.internet.MimeMultipart;
 import javax.mail.search.FlagTerm;
 
 
+
 public class Reciver {
-	
-	//String FOLDER_INDOX = "INBOX"; // имя папки "Входящие"
+
 	String SSL_FACTORY = "javax.net.ssl.SSLSocketFactory";
 	private Properties pop3Props;
 	private URLName url;
 
 	public Reciver(String username, String password) {
 		pop3Props = new Properties();
-		
+
 		pop3Props.setProperty("mail.pop3.socketFactory.class", SSL_FACTORY);
 		pop3Props.setProperty("mail.pop3.socketFactory.fallback", "false");
 		pop3Props.setProperty("mail.pop3.port", "995");
 		pop3Props.setProperty("mail.pop3.socketFactory.port", "995");
 		url = new URLName("pop3", "pop.gmail.com", 955, "", username, password);
 	}
-	
+
 	public void recive(String folderInbox) throws MessagingException, IOException {
-	
+
 		Session session = Session.getInstance(pop3Props, null);
 		Store store = session.getStore(url);
 		store.connect();
 
 		Folder folder = store.getFolder(folderInbox);
-
+		
 		try {
 
 			folder.open(Folder.READ_WRITE);
@@ -40,35 +43,44 @@ public class Reciver {
 			folder.open(Folder.READ_ONLY);
 
 		}
-		
+
 		Message[] messages = folder.search(new FlagTerm(new Flags(Flags.Flag.SEEN), false));
-		
+
 		for (int i = 0; i < messages.length; i++) {
-			String subject = messages[i].getSubject().toString().toLowerCase().trim(); //Получение темы письма
+			String subject = messages[i].getSubject().toString().toLowerCase().trim(); // Получение темы письма
+																						
+			String letterConfirmationSubject = "your order has been received"; // Тема письма которое содержит сonfirmation number
+																				
 
-			String letterConfirmationSubject = "this is subject"; //Тема письма которое содержит активационную ссылку
-
-                        //Выбор нужного письма
+			// Выбор нужного письма
 			if (!subject.equals(letterConfirmationSubject)) {
-				System.err.println("Сообщение пропущено, т.к. не является активационным письмом.......");
+				System.err.println("Сообщение пропущеноooo, т.к. не является активационным письмом.......");
+
+				System.out.println(subject);
+				System.out.println("1" + messages.length);
 				continue;
 			}
 
 			System.out.println("Сообщение принято к обработке");
-			
-			Object contentMessage = messages[i].getContent();
-			String content = contentMessage.toString();
-                        
-                        //Проверка наличия активационной ссылки в теле письма
-			CharSequence searchPhrase = "This is text";
-			if (content.contains(searchPhrase)) {
-				System.out.println("ОК.Письмо содержит активационную ссылку.");
-			} else {
-				System.err.println("!!!===SHIT!Письмо не содержит активационную ссылку===!!!");
-				
-		}
-		folder.close(false);
 
-		store.close();}}}
-	
-		
+
+			String result = "";
+			MimeMultipart mimeMultipart = (MimeMultipart) messages[i].getContent();
+			BodyPart bodyPart = mimeMultipart.getBodyPart(i);
+			String html = (String) bodyPart.getContent();
+			result = result + "\n" + org.jsoup.Jsoup.parse(html).text();
+			CharSequence searchPhrase = "070756246742";
+			if (html.contains(searchPhrase)) {
+				System.out.println("ОК.Mail boby contains confirmation number.");
+			} else {
+				System.err.println("!!!===SHIT!Mail boddy do not contains confirmation number===!!!");
+
+			}
+			System.out.println(result);
+
+			folder.close(false);
+
+			store.close();
+		}
+	}
+}
